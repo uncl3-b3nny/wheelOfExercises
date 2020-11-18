8942bc2cb9f264269c27d0e83f552ba85ad989a2
uncl3_b3nny <bjlinville1@gmail.com>
2020-11-18 13:23:46 -0500

    got the wheel to show reflect the names of the exercises in the table. Also got it to spin & click

--- resources/views/exercises/index.blade.php
+++ resources/views/exercises/index.blade.php
@@ -1,6 +1,14 @@
+
 @extends('layouts.app')
 
 @section('content')
+   <button onClick="theWheel.startAnimation();">Spin the Wheel!</button>
+
+    <br>      
+    <canvas id='canvas' width='880' height='420'>
+        Canvas not supported, use another browser.
+    </canvas>
+
     <br>
     <div class="row">
         <div class="col-lg-12 margin-tb">
@@ -51,13 +59,52 @@
     </table>
   
     {!! $exercises->links() !!}
-      
-    <canvas id='canvas' width='880' height='300'>
-        Canvas not supported, use another browser.
-    </canvas>
-    
-    <script>
-        let theWheel = new Winwheel();
-    </script>
+
+<script>
+    // @ToDo: 3
+    // set the number of wheel segments to the number of exercises
+    var exercises = {!! json_encode($exercises) !!}
+    var number_of_segments = exercises.data.length
+
+    // put the exercise names in an array
+    var names = []
+    exercises.data.forEach(element => names.push(element.name));
+
+    // make an array of colors
+    var colors = ['#ee1c24','#3cb878','#f6989d','#00aef0','#f26522','#e70697','#fff200','#f6989d','#ee1c24','#3cb878','#f26522','#a186be','#fff200','#00aef0','#ee1c24','#f6989d','#f26522','#3cb878','#000000','#a186be','#fff200','#00aef0','#ffffff'];
+
+    // put them together in an array of objects so Winwheel can use it to paint & name the segments correctly
+    var segments = []
+    names.forEach(function(name, index){ segments.push({fillStyle:colors[index], text:name})});
+</script>
+
+<script>
+    let theWheel = new Winwheel({
+        'numSegments' : number_of_segments,
+        'segments'    : segments,
+        'outerRadius' : 170,
+        'animation' :
+        {
+            'type'          : 'spinToStop',
+            'duration'      : 10,
+            'spins'         : 4,
+            'callbackSound' : playSound,    // Specify function to call when sound is to be triggered
+        }
+    });
+ 
+    let audio = new Audio('tick.mp3');  // Create audio object and load desired file.
+ 
+    function playSound()
+    {
+        // Stop and rewind the sound (stops it if already playing).
+        audio.pause();
+        audio.currentTime = 0;
+ 
+        // Play the sound.
+        audio.play();
+    }
+</script>
+
+
 @endsection
 
--- resources/views/layouts/App.blade.php
+++ resources/views/layouts/App.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Spin the Wheel!</title>
+    <title>Free Wheelin</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
-    <script src="{{ asset('js/Winwheel.js')  }}"></script>
-
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+    <script src="{{ asset('js/Winwheel.js') }}"></script>
 </head>
 <body>
   
